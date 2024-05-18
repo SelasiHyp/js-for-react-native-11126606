d461a06a14c4876f8ebe15b4b82f2d7d54113368
SelasiHyp <psyaovi@st.ug.edu.gh>
2024-05-18 12:19:17 +0000

    Task2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,22 @@ function processArray(arr) {
         }
     }return result;
 }
+
+function  formatArrayStrings(string, processedNum){
+    for(var i =0; i<string.length;i++){
+        if(processedNum[i] % 2 ===0){
+            string[i]= string[i].toUpperCase();
+        }else{
+            string[i]= string[i].toLowerCase();
+        }
+    }return string;
+
+}
+
+/*
+const strings = ["hello","hypeman", "world", "john", "car", "GOOD MORNING"];
+const numbers = [30, 121, 33, 7, 80];
+const processedNumbers = processArray(numbers);
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(formattedStrings);
+*/
